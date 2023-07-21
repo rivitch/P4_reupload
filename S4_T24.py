a8fcbef5f15f7793733884cbe148a799be6be372
«rivitch» <rivitch@rambler.ru>
2023-07-22 03:56:19 +0400

    remade 24

--- S4_T24.py
+++ S4_T24.py
@@ -1,11 +1,16 @@
 print("")
 print("Задача 24")
-
-N = int(input("Введите количество кустов :")) 
-c = int(input("Введите номер куста :"))-1
+N = c1 = ""
+while N.isdigit()==False or c1.isdigit()==False :
+    try:
+        N = input("Введите количество кустов :")
+        c1 = input("Введите номер куста :")        
+    except ValueError:
+        print("Ошибка ввода. Введите число.")
 from random import randint
-list_1 = list() 
-for i in range(N): 
+list_1 = list()
+c = int(c1) 
+for i in range(int(N)): 
     n = randint(0, 1000) 
     list_1.append(n) 
 if c+1 == len(list_1):
@@ -13,5 +18,6 @@ if c+1 == len(list_1):
 else:
     q = list_1[c+1]
 print(list_1[c-1]+list_1[c]+q)
+# Проверка
 # print(list_1)
 # print(list_1[c-1],list_1[c] ,q,(list_1[c-1]+list_1[c]+q))
